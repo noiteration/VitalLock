72938a1b225db9feece231a7ba6608525990f75a
Sahir <47252018+noiteration@users.noreply.github.com>
2025-02-23 00:24:11 -0500

    Vitallock keys model created.

---
+++ frontend/app/Http/Controllers/VLKeyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVLKeyRequest;
+use App\Http\Requests\UpdateVLKeyRequest;
+use App\Models\VLKey;
+
+class VLKeyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVLKeyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VLKey $vLKey)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VLKey $vLKey)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVLKeyRequest $request, VLKey $vLKey)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VLKey $vLKey)
+    {
+        //
+    }
+}
